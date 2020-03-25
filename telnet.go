5faf06c2b41e9b3a40bfad8f47eb03c246d348f6
Vitalii Laut <mclaut@mclaut.com>
2020-03-25 13:01:38 +0200

    add telnet function

---
+++ telnet.go
@@ -0,0 +1,32 @@
+package common
+
+import (
+	"net"
+	"strings"
+)
+
+func telnetCommand(addr, cmd string) (string, error) {
+	var answ string
+	// connect to unix socket
+	c, err := net.Dial("unix", addr)
+	if err != nil {
+		return answ, err
+	}
+
+	// send command to unix socket
+	_, err = c.Write([]byte(cmd + "\n"))
+	if err != nil {
+		return answ, err
+	}
+
+	// get answer from unix socket
+	b := make([]byte, 512)
+	n, err := c.Read(b)
+	if err != nil {
+		return answ, err
+	}
+
+	str := strings.TrimRight(string(b[:n]), " \r\n")
+
+	return str, nil
+}
